8c78ad32122e772b2b37048d2f37e7663c86805e
zhangwei812 <1733773547@qq.com>
2023-03-02 23:17:17 +0800

    zecrey_treasure_hunt  configFile

--- model/model.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Player struct {
-	AccountIndex int64  `gorm:"index" json:"account_index"`
-	AccountName  string `json:"account_name" gorm:"index;not null;size:128"`
-	L2publicKey  string `json:"l2public_key"`
-}
